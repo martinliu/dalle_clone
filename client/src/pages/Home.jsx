b8d3c34d9f71e27019926ee5c167597fc7e62a75
Martin Liu <liuzh66@gmail.com>
2023-02-05 23:22:33 +0800

    change backend url

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
         setLoading(true);
 
         try {
-            const response = await fetch('https://dall-e-backend-4zil.onrender.com/api/v1/post', {
+            const response = await fetch('https://dell-e-frontend.onrender.com/api/v1/post', {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
